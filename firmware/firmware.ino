93c073d3defc72c775afb2560120c47981a23d65
Max Henstell <max@kapamaki.net>
2013-04-12 18:36:22 +0800

    Firmware extended to support bi-directional communication

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,20 +1,7 @@
 #include <SoftwareSerial.h>
 
-#define expectedACKLength 5
-uint8_t expectedACK[expectedACKLength] = {0x02, 0x80, 0x01, 0x00, 0x81};
-
-#define BLE_ACK_LENGTH 6
-char bleACK[BLE_ACK_LENGTH];
-
-int transmitCount;
-int receiveErrorCount;
-
-#define MAXIMUM_RECEIVE_ERRORS_FOR_FAIL 2
-#define MINIMUM_TRANSMISSIONS_FOR_PASS 3
-
 #define SOFTWARE_RX_PIN 2
 #define SOFTWARE_TX_PIN 3
-
 
 #define LED_RED   6
 #define LED_BLUE  9
@@ -23,9 +10,14 @@ int receiveErrorCount;
 #define RESET_PIN     7
 #define CONNECTED_PIN 4
 
-#define SWITCH_A 8
-#define SWITCH_B 10
+#define SWITCH_A 10
+#define SWITCH_B 8
 
+uint8_t expectedTransmitACK[5] = {0x02, 0x80, 0x01, 0x00, 0x81};
+uint8_t expectedRecieveACK[3] = {0x02, 0x20, 0xDD};
+
+uint8_t count = 0;
+long lastNotifyTime = millis();
 
 SoftwareSerial mySerial(SOFTWARE_RX_PIN, SOFTWARE_TX_PIN);
 
@@ -35,23 +27,38 @@ void setLED(uint8_t R, uint8_t G, uint8_t B) {
   analogWrite(LED_BLUE,   B);
 }
 
-void sendDATA(char *str){
-  if(transmitCount >= MINIMUM_TRANSMISSIONS_FOR_PASS) {
-    setLED(0,255,0);  // All good!
+void read_ble(uint8_t buffer[]) {
+  // Zero the buffer
+  for(int x = 0; x < 64; x++) {
+    buffer[x] = 0x00;
   }
+
+  // Make sure we got all the data
+  delay(50);
+
+  // Read all the data   
+  for(int x=0; x<64; x++) {
+    buffer[x] = mySerial.read();
+  }  
+}
+
+void BLE_Send(char *str){
+  setLED(0,255,0);  // All good!
   
+  boolean error = false;
+
   int exOR_result = 0xFD;
   int dataLength = 0;
-  
 
   // Calculate the length of the message
   for(dataLength=0; str[dataLength]!=0; dataLength++);
-  
+
   // We can handle a maximum of 20 bytes, so bail if it's too long
   if(dataLength > 20) {
+    setLED(0,0,0);
     return;
   }
-  
+
   // Flush the software serial buffer, in case there was stuff in it
   mySerial.flush();
   
@@ -68,45 +75,112 @@ void sendDATA(char *str){
     exOR_result = exOR_result ^ str[counter];
   }
   mySerial.write(exOR_result);
-
-  int receivedBytes = mySerial.readBytes(bleACK, 6); // Ack is 5, Nack is 6
   
-  if(receivedBytes != 5) {
-    receiveErrorCount++;
-  }
+  // Wait for a reply
+  delay(50);
+  
+  uint8_t buffer[64];
+  
+  // Read reply and check for a positive ACKnowledgement
+  if (mySerial.available() != 5) {
+    error = true;
+  } 
   else {
-    for (uint8_t i = 0; i < expectedACKLength; i++) {
-      if ((uint8_t)bleACK[i] != expectedACK[i]) {
-        receiveErrorCount++;
+    
+    read_ble(buffer);
+    
+    for (uint8_t i = 0; i < sizeof(expectedTransmitACK); i++) {
+      if (buffer[i] != expectedTransmitACK[i]) {
+        error = true; 
       }
     }
   }
   
-  while(receiveErrorCount >= MAXIMUM_RECEIVE_ERRORS_FOR_FAIL) {
-    setLED(255,0,0);
-    delay(500);    
-    setLED(0,0,0);
-    delay(500);
+  // Handle a data send failure
+  if(error) {
+    Serial.println("Error sending data to the BLE module.");
+    dump_buffer(buffer);
+    error_flash();
+    return;
   }
 
-  char buff[30];
-  snprintf(buff, 30, "%i, %x,%x,%x,%x,%x\n", receivedBytes,
-                 bleACK[0],bleACK[1],bleACK[2],bleACK[3],bleACK[4]);
-  Serial.print(buff);
-  
-  transmitCount++;
+  setLED(0,0,0);
 }
 
+// Recieve a message from the BLE shield
+int BLE_Recieve(char* inbuffer) {
+  uint8_t buffer[64];
+  boolean error = false;
+  
+  setLED(0,0,255);
+
+  // Zero the buffer
+  for(int x = 0; x < 64; x++) {
+    buffer[x] = 0x00;
+    inbuffer[x] = 0x00;
+  }
+
+  // Make sure we got all the data
+  delay(50);
+
+  // Read all the data   
+  read_ble(buffer);  
+  
+  // Make sure the header is as expected
+  for (int i = 0; i < 3; i++) {
+    if (buffer[i] != expectedRecieveACK[i]) {
+        error = true;
+    }
+  }
+  
+  setLED(0, 0, 0);
+  
+  // Handle recieve errors. TODO: XOR Checksum
+  if (error == true) {
+    Serial.println("Error receiving data from the BLE module.");
+    dump_buffer(buffer);
+    error_flash();
+    return 0;
+  }
+  
+  // Copy the payload bytes to the provided char array
+  int length = buffer[3];
+  char payload[length];
+  for (int x = 0; x < length; x++) {
+    inbuffer[x] = (char)buffer[4 + x];
+  }
+  
+  return length;
+}
+
+// Debugging
+void dump_buffer(uint8_t buffer[]) {
+  for (int x=0; x < 64; x++) {
+    Serial.print(buffer[x], HEX);
+    Serial.print(" ");
+  }
+  Serial.println();
+}
+
+// Flash the RED Led
+void error_flash() {
+ for (int x=0; x < 4; x++) {
+    setLED(255,0,0);
+    delay(75);    
+    setLED(0,0,0);
+    delay(75);
+  }
+}
+
+
 void setup() { 
- //Initialize serial and wait for port to open:
+  //Initialize serial and wait for port to open:
   pinMode(SOFTWARE_RX_PIN, INPUT);
   pinMode(SOFTWARE_TX_PIN, OUTPUT);
   mySerial.begin(9600);
   mySerial.listen();
-  mySerial.setTimeout(1000);
-  receiveErrorCount = 0;
 
-  Serial.begin(9600);
+  Serial.begin(115200);
 
   // Set the switch pins to inputs
   pinMode(CONNECTED_PIN, INPUT);  
@@ -115,7 +189,7 @@ void setup() {
 
   // Disable the RGB led
   setLED(20,0,0);
-  
+
   // Enable the reset output
   digitalWrite(RESET_PIN, HIGH);
   pinMode(RESET_PIN, OUTPUT);
@@ -125,32 +199,52 @@ void setup() {
   delay(100);
   digitalWrite(RESET_PIN, HIGH);  
 
-//  while (!Serial) {
-//    ; // wait for serial port to connect. Needed for Leonardo only
-//  }
+  //  while (!Serial) {
+  //    ; // wait for serial port to connect. Needed for Leonardo only
+  //  }
+  
 }
 
 void loop() {
-  unsigned int count = 0;
-  char message[20];
+
+  // While we are connected, send a test sequence every second
+  if(digitalRead(CONNECTED_PIN) && (millis() - lastNotifyTime > 1000)) {
+    setLED(20,20,20);  // white: connected  
     
-  // While we are connected, send a test sequence
-  while(digitalRead(CONNECTED_PIN)) {
-    setLED(20,20,20);  // white: connected
-    
+    char message[20];
     snprintf(message, 20, "Connected %is", count);
-    sendDATA(message);
+    BLE_Send(message);
+
+    Serial.println(message);
     count++;
-    delay(1000);
+    
+    lastNotifyTime = millis();
+    setLED(0,0,0);
   }
   
-  if(digitalRead(SWITCH_A) == 0) {
-    setLED(0,20,0); //green: switch a pressed
+  // Check to see if we've received any data from the BLE shield
+  if (mySerial.available() > 0) {
+    char buffer[64];
+    int length = BLE_Recieve(buffer);
+    Serial.print("Message recieved: ");
+    Serial.println(buffer);
   }
-  else if(digitalRead(SWITCH_B) == 0) {
+
+  if(digitalRead(SWITCH_A) == LOW) {
+    setLED(0,20,0); //green: switch a pressed
+    BLE_Send("Button A!");
+  }
+  else if(digitalRead(SWITCH_B) == LOW) {
     setLED(0,0,20); // blue: switch b pressed
+    BLE_Send("Button B!");
+  }
+  else if(digitalRead(CONNECTED_PIN) == LOW) {
+    setLED(127,0,0); 
   }
   else {
-    setLED(20,0,0);   // Red: disconnected
+    setLED(0,0,0); 
   }
+  
+  
 }
+
